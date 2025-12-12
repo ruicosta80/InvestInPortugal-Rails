0cc78c9e61ae419e6d740f3d4495c16f1179630b
Rui Costa <ruicosta80@gmail.com>
2025-12-12 15:41:31 +0200

    Final Fix: Explicitly defined after_sign_out_path_for to resolve session persistence after logout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,13 @@ class ApplicationController < ActionController::Base
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
+  # Forces redirect after sign out to the Admin sign-in page,
+  # which prevents the session cookie persistence issue.
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin_user
+      new_admin_user_session_path
+    else
+      super
+    end
+  end
 end
